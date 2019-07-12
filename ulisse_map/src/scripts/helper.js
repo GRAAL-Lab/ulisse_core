c58a037e92944f22e54ee4cd148950d16a93e1c5
Patrick Roncagliolo <ronca.pat@gmail.com>
2019-07-12 15:43:51 +0200

    Basic NURBS encoding

--- ulisse_map/src/scripts/helper.js
+++ ulisse_map/src/scripts/helper.js
@@ -217,3 +217,36 @@ function lon_to_m_coeff(lon){
     lon = deg_to_rad(lon)
     return 111412.84*Math.cos(lon) - 93.5*Math.cos(3*lon) + 0.118*Math.cos(5*lon)
 }
+
+
+function serialize(object, maxDepth) {
+    function _processObject(object, maxDepth, level) {
+        var output = []
+        var pad = "  "
+        if (maxDepth == undefined) {
+            maxDepth = -1
+        }
+        if (level == undefined) {
+            level = 0
+        }
+        var padding = new Array(level + 1).join(pad)
+
+        output.push((Array.isArray(object) ? "[" : "{"))
+        var fields = []
+        for (var key in object) {
+            var keyText = Array.isArray(object) ? "" : ("\"" + key + "\": ")
+            if (typeof (object[key]) == "object" && key != "parent" && maxDepth != 0) {
+                var res = _processObject(object[key], maxDepth > 0 ? maxDepth - 1 : -1, level + 1)
+                fields.push(padding + pad + keyText + res)
+            } else {
+                fields.push(padding + pad + keyText + "\"" + object[key] + "\"")
+            }
+        }
+        output.push(fields.join(",\n"))
+        output.push(padding + (Array.isArray(object) ? "]" : "}"))
+
+        return output.join("\n")
+    }
+
+    return _processObject(object, maxDepth)
+}
