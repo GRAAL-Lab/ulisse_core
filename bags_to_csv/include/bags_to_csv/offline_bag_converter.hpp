93ddca0a89f82d6db16c006172fe1b91a7627d85
Francesco Wanderlingh <fwanderlingh@gmail.com>
2021-05-11 13:55:09 +0200

    - Added functions to offline bsg converter

--- bags_to_csv/include/bags_to_csv/offline_bag_converter.hpp
+++ bags_to_csv/include/bags_to_csv/offline_bag_converter.hpp
@@ -1,11 +1,26 @@
 #ifndef OFFLINEBAGCONVERTER_HPP
 #define OFFLINEBAGCONVERTER_HPP
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/clock.hpp"
+#include "rclcpp/serialization.hpp"
+#include "rclcpp/serialized_message.hpp"
+
+#include "rcpputils/filesystem_helper.hpp"
+#include "rcutils/time.h"
+
+#include "rosbag2_cpp/reader.hpp"
+#include "rosbag2_cpp/readers/sequential_reader.hpp"
 
 class OfflineBagConverter
 {
 public:
     OfflineBagConverter();
+
+    void ConvertToCSV(const std::string& bag_folder, const std::string& csv_folder);
 };
 
 #endif // OFFLINEBAGCONVERTER_HPP
--- bags_to_csv/src/bags_to_csv/offline_bag_converter.cpp
+++ bags_to_csv/src/bags_to_csv/offline_bag_converter.cpp
@@ -1,6 +1,59 @@
+#include <iostream>
+#include <cstdio>
+
+#include "std_msgs/msg/string.hpp"
+
 #include "bags_to_csv/offline_bag_converter.hpp"
 
 OfflineBagConverter::OfflineBagConverter()
 {
 
+}
+
+void OfflineBagConverter::ConvertToCSV(const std::string& bag_folder, const std::string& csv_folder)
+{
+    using TopicMsgT = std_msgs::msg::String;
+
+    auto rosbag_directory = rcpputils::fs::path(bag_folder);
+
+    rosbag2_cpp::Reader reader(std::make_unique<rosbag2_cpp::readers::SequentialReader>());
+
+    rosbag2_cpp::StorageOptions storage_options{};
+    storage_options.uri = bag_folder;
+    storage_options.storage_id = "sqlite3";
+
+    rosbag2_cpp::ConverterOptions converter_options{};
+    converter_options.input_serialization_format = "cdr";
+    converter_options.output_serialization_format = "cdr";
+
+    reader.open(storage_options, converter_options);
+
+    auto topics = reader.get_all_topics_and_types();
+
+    for (auto&& t : topics){
+        std::cout << "meta name: " << t.name << std::endl;
+        std::cout << "meta type: " << t.type << std::endl;
+        std::cout << "meta serialization_format: " << t.serialization_format << std::endl;
+    }
+
+    // read and deserialize "serialized data"
+    while (reader.has_next()) {
+        auto bag_message = reader.read_next();
+
+        std::cout<<"Found topic name " << bag_message->topic_name << std::endl;
+
+        if (bag_message->topic_name == "/topic") {
+
+            TopicMsgT extracted_test_msg;
+            rclcpp::Serialization<TopicMsgT> serialization;
+            rclcpp::SerializedMessage extracted_serialized_msg(*bag_message->serialized_data);
+            serialization.deserialize_message(
+                &extracted_serialized_msg, &extracted_test_msg);
+
+            std::cout<<"Found data in topic " << bag_message->topic_name << ": " << extracted_test_msg.data << std::endl;
+
+
+        }
+
+    }
 }
