4bf90c8e870e0ff9ec3c769f18427dec719e2f79
Francesco Wanderlingh <fwanderlingh@gmail.com>
2023-01-20 13:53:09 +0100

    - Rolled back to ROS2 foxy

--- ulisse_ctrl/src/ulisse_ctrl/path_manager.cpp
+++ ulisse_ctrl/src/ulisse_ctrl/path_manager.cpp
@@ -34,7 +34,7 @@ bool PathManager::Initialization(const ulisse_msgs::msg::PathData& path)
     std::cout << "nurbsParam.deltaMax: " << nurbsParam.deltaMax << std::endl;
 
     std::cout.precision(10);
-    std::cout  << "Path Message:\n" << rosidl_generator_traits::to_yaml(path) << std::fixed << std::endl;
+    //std::cout  << "Path Message:\n" << rosidl_generator_traits::to_yaml(path) << std::fixed << std::endl;
 
     pathName_ = path.id;
     pathType_ = path.type;
